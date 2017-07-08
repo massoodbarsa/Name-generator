f4d358b67a22e5ac15a75f3c9f127fdb2132cb22
Joost Lubach <joostlubach@gmail.com>
2017-07-08 22:36:50 +0200

    Added name generator base

---
+++ name-generator/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import store from '../store';
+import {IncreaseButton} from '.'
+import './App.css';
+
+class App extends Component {
+
+  state = store.state;
+
+  componentDidMount() {
+    store.subscribe(this.onStateChange);
+  }
+
+  componentWillUnmount() {
+    store.unsubscribe();
+  }
+
+  onStateChange = newState => {
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-state">
+          {JSON.stringify(this.state)}
+        </div>
+
+        <IncreaseButton/>
+      </div>
+    );
+  }
+}
+
+export default App;
